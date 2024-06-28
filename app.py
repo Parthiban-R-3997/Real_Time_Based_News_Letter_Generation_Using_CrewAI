ae0a8540ed7c93d3493b3bd5553d3a084c5652f9
Parthiban-R-3997 <rparthiban729@gmail.com>
2024-06-28 13:52:02 +0100

    Update

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 import os
 import sys
-sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), 'src')))
+#sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), 'src')))
 
 import streamlit as st
 from datetime import datetime, timedelta
